7fe4ae18757301c87d642cbe8f20f2510f160d4d
koushiksai6355-sketch <koushiksai6355@gmail.com>
2026-05-10 16:33:56 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = "https://task-manager-production-20a5.up.railway.app"
 
 st.set_page_config(page_title="Team Task Manager", layout="wide")
 
